9d6e528523d3c16428d1e2540377464fb37eb32f
anton.chernets <anton.chernets@artpix3d.com>
2021-03-10 12:50:31 +0200

    1. Получить сумму всех вторых элементов массива произвольного размера и вложенности.

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
-var_dump(array(
-    'home_work_1' => true
-));
+//1. Получить сумму всех вторых элементов массива произвольного размера и вложенности.
+$array = [1,2,5,5,2,3];
+
+//One example
+$sumEvenNumbers = 0;
+for ($i = 1; $i <= count($array); $i++) {
+    if ($i % 2 === 0) {
+        $sumEvenNumbers += $array[$i-1];
+    }
+}
+var_export($sumEvenNumbers);
+
+//Two example
+$sumEvenNumbers2 = 0;
+foreach ($array as $key => $value){
+    if ($key === 0 && $key % 2 === 0) {
+        $sumEvenNumbers2 += $value;
+    }
+}
+var_export($sumEvenNumbers);
