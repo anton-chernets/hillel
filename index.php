a81d04ccd70ea30379c2ea22ecc75ffe21b13a30
anton.chernets <anton.chernets@artpix3d.com>
2021-03-10 13:24:59 +0200

    2. Определить количество символов входящих в произвольную строку, т.е у строки 'afrae' a = 2, e=1, f=1,r=1

--- index.php
+++ index.php
@@ -20,3 +20,23 @@ foreach ($array as $key => $value){
     }
 }
 var_export($sumEvenNumbers);
+
+/*2.*/ $str = "Определить количество символов входящих в произвольную строку, т.е у строки 'afrae' a = 2, e=1, f=1,r=1";
+
+//$str = 'Some text for example';
+$result = [];
+if(preg_match('/[а-я]+/msi', $str)){
+    echo 'Cyrillic not supported';
+} else{
+    for ($i = 0; $i <= strlen($str); $i++) {
+        $key = $str[$i];
+        if(!empty($key)){
+            if(array_key_exists($key, $result)){
+                ++$result[$key];
+            } else {
+                $result[$key] = 1;
+            }
+        }
+    }
+    var_dump($result);
+}
